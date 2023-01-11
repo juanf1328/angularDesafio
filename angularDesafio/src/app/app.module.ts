3ac09915632658a133d5972123e9829c7fc52e52
juanf1328 <jforni@encodesa.com.ar>
2023-01-11 16:56:47 -0300

    components

--- angularDesafio/src/app/app.module.ts
+++ angularDesafio/src/app/app.module.ts
@@ -5,15 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../pages/login/login.component';
+import { AdminComponent } from '../pages/admin/admin.component';
+import { EmpleadoComponent } from '../pages/empleado/empleado.component';
+import { ErrorComponent } from '../pages/error/error.component';
+import { OptionsComponent } from '../pages/options/options.component';
+import { RrhhComponent } from '../pages/rrhh/rrhh.component';
+import { SeguridadComponent } from '../pages/seguridad/seguridad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    EmpleadoComponent,
+    ErrorComponent,
+    OptionsComponent,
+    RrhhComponent,
+    SeguridadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
